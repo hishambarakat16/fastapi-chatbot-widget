6c9156113e83d8415593685493318b2e7e4e5706
Hisham Barakat <hishambarakat16@gmail.com>
2026-01-18 01:04:47 +0800

    oragnized css files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles.css";
+import "./styles/tokens.css";
+import "./styles/base.css";
+import "./styles/layout.css";
+import "./styles/widget.css";
+import "./styles/inspector.css";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
